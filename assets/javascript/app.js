80807b83bde57f11cacfa735434ae26ef16999dc
sharebot-joe <josephikim2@gmail.com>
2018-04-05 16:59:22 -0700

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,63 +1,149 @@
 $(document).ready(function() {
-	var apiKey = 'tj4h4gdxevqPIJTUM8kTJjt91EWJTm9D'
+	// Global objects
+  var apiKey = 'tj4h4gdxevqPIJTUM8kTJjt91EWJTm9D'
 	var limit = 10
 	var topics = ['Dave Chappelle', 'Dana Carvey', 'Jon Mulaney', 'Bill Hader', 'Robin Williams', 'Chris Rock', 'Louis CK', 'Aziz Ansari', 'Jerry Seinfeld', 'George Carlin']
-	var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&limit=' + limit + '&q='
-	function initializeButtons() {
-		for (let text of topics)
-  		$('<button></button').text(text).addClass('btn btn-info').appendTo('.buttons')
+	var queryBaseURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&limit=' + limit + '&q='
+	
+  // Prototypes 
+  // Converts a string to Title Case
+  String.prototype.toTitleCase = function () {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  };
+  // Functions
+
+  function initializeButtons() {
+	for (let text of topics)
+  	$('<button></button').text(text).addClass('btn btn-info').appendTo('.buttons')
 	}
 
 	// Main Program
 	initializeButtons();
 
 	// 	When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-	 $(document).on('click', '.btn', function() {
-    fired_button = $(this).text();
+	$(document).on('click', '.buttons .btn', function() {
+    var fired_button = $(this).text();
    
     var string = encodeURIComponent(fired_button);
    
-    queryURL = queryURL + string
+    var queryURL = queryBaseURL + string
+
     // ajax call 
     $.ajax({
     	url: queryURL,
     	method: "GET"
     }).then(function(response) {
+        $('.main').empty();
     	console.log('queryURL: ' + queryURL);
     	console.log(response)
+
+        // For each element of the response's data object...
     	response.data.forEach(function(gifObject){
 
+            // Creating new div
     		var gifDiv = $('<div class = \'gifdiv\'></div>')
 
     		var rating = $('<p>Rating: ' + gifObject.rating + '</p>')
     		
+            //Creating placeholder image
     		var img = $('<img>')
-    		img.attr('src', gifObject.images['480w_still'].url)
-    		img.click(function() {
-    			var gifURL = gifObject.images.fixed_height.url
-					img.attr("data-gif", gifURL);
-					img.attr("id", "gif-img");
-					var tempUrl = img.attr("data-gif");
-					$("#gif-img").attr("data-gif") = $("#gif-img").attr("src");	
-					$("#gif-img").attr("src") = tempUrl;
-				});
+    		img.attr({src:gifObject.images['480w_still'].url, height:200})
 
-				gifDiv.append(rating)
+            // Function that toggles gif playback via click
+    		img.click(function() {
+                var currentSrc = img.attr('src')
+                var stillURL = gifObject.images['480w_still'].url
+    			var gifURL = gifObject.images.fixed_height.url
+
+                if (currentSrc.endsWith('jpg')) {
+                    img.attr('src', gifURL)
+                } else if (currentSrc.endsWith('gif')) {
+                    img.attr('src', stillURL)
+                }
+            });
+
+            // Bundle up new div
+			  gifDiv.append(rating)
     		gifDiv.append(img)
 
+            // Add new div to main section 
     		$('.main').append(gifDiv)
-    		// Toggles playback of gif via button click
-				// $('.gifdiv').click(function() {
-				// 	killerImg.attr("data-gif", gifURL);
-				// 	killerImg.attr("id", "killer-img");
-				// 	var tempUrl = $("#killer-img").attr("data-gif");
-				// 	$("#killer-img").attr("data-gif") = $("#killer-img").attr("src");	
-				// 	$("#killer-img").attr("src") = tempUrl
-				// });
     		
     	})
     })
   });
+
+  $('input[type="submit"]').on("click", function(event) {
+    // Preventing the buttons default behavior when clicked (which is submitting a form)
+    event.preventDefault();
+    
+    var input = $("#userinput").val().trim();
+    console.log(input)
+    // This line checks for input from the textbox
+    if (input != '' && input != $("#userinput").defaultValue) {
+      
+      // Make new queryURL
+      var string = encodeURIComponent(input);
+      var queryURL = queryBaseURL + string
+      console.log('new queryURL: ' + queryURL)
+
+      // ajax call 
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        topics.push(input.toTitleCase())
+        console.log(topics)
+        $('.buttons').empty();
+        initializeButtons();
+      })
+    }
+  });
+      //   $('.main').empty();
+      //   console.log('queryURL: ' + queryURL);
+      //   console.log(response)
+
+      //     // For each element of the response's data object...
+      //   response.data.forEach(function(gifObject){
+
+      //         // Creating new div
+      //     var gifDiv = $('<div class = \'gifdiv\'></div>')
+
+      //     var rating = $('<p>Rating: ' + gifObject.rating + '</p>')
+          
+      //         //Creating placeholder image
+      //     var img = $('<img>')
+      //     img.attr({src:gifObject.images['480w_still'].url, height:200})
+
+      //         // Function that toggles gif playback via click
+      //     img.click(function() {
+      //             var currentSrc = img.attr('src')
+      //             var stillURL = gifObject.images['480w_still'].url
+      //       var gifURL = gifObject.images.fixed_height.url
+
+      //             if (currentSrc.endsWith('jpg')) {
+      //                 img.attr('src', gifURL)
+      //             } else if (currentSrc.endsWith('gif')) {
+      //                 img.attr('src', stillURL)
+      //             }
+      //         });
+
+      //         // Bundle up new div
+      //     gifDiv.append(rating)
+      //     gifDiv.append(img)
+
+      //         // Add new div to main section 
+      //     $('.main').append(gifDiv)
+          
+      //   })
+      // })
+      // Adding the input from the textbox to our array
+      
+
+    
+    // $('<button></button').text(text).addClass('btn btn-info').appendTo('.buttons')
+    // // Calling renderButtons which handles the processing of our movie array
+    // renderButtons();
 
 
 // Only once you get images displaying with button presses should you move on to the next step.
