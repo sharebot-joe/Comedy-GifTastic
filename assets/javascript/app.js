b4fb890cac77f85cb020c461edc59f2782732217
sharebot-joe <josephikim2@gmail.com>
2018-04-05 17:10:45 -0700

    clean up index.html formatting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -99,56 +99,5 @@ $(document).ready(function() {
       })
     }
   });
-      //   $('.main').empty();
-      //   console.log('queryURL: ' + queryURL);
-      //   console.log(response)
-
-      //     // For each element of the response's data object...
-      //   response.data.forEach(function(gifObject){
-
-      //         // Creating new div
-      //     var gifDiv = $('<div class = \'gifdiv\'></div>')
-
-      //     var rating = $('<p>Rating: ' + gifObject.rating + '</p>')
-          
-      //         //Creating placeholder image
-      //     var img = $('<img>')
-      //     img.attr({src:gifObject.images['480w_still'].url, height:200})
-
-      //         // Function that toggles gif playback via click
-      //     img.click(function() {
-      //             var currentSrc = img.attr('src')
-      //             var stillURL = gifObject.images['480w_still'].url
-      //       var gifURL = gifObject.images.fixed_height.url
-
-      //             if (currentSrc.endsWith('jpg')) {
-      //                 img.attr('src', gifURL)
-      //             } else if (currentSrc.endsWith('gif')) {
-      //                 img.attr('src', stillURL)
-      //             }
-      //         });
-
-      //         // Bundle up new div
-      //     gifDiv.append(rating)
-      //     gifDiv.append(img)
-
-      //         // Add new div to main section 
-      //     $('.main').append(gifDiv)
-          
-      //   })
-      // })
-      // Adding the input from the textbox to our array
-      
-
-    
-    // $('<button></button').text(text).addClass('btn btn-info').appendTo('.buttons')
-    // // Calling renderButtons which handles the processing of our movie array
-    // renderButtons();
-
-
-// Only once you get images displaying with button presses should you move on to the next step.
-
-// Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
-
 
 });
